4412ee8a86a858249c4af3f987d63c3d759baead
KukinIvan <Provokatorx3@gmail.com>
2022-08-24 16:56:27 +0300

    add 16.9.1

---
+++ 16.9.2.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    def __init__(self, a,b):
+        self.a = a
+        self.b = b
+
+    def get_area(self):
+        return self.a*self.b
+ 
+rect_1 = Rectangle(5,10)
+print(rect_1.get_area())
